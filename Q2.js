9bb7e7e58f47db66c70419a6642910b1428bc4d5
Sharath-Chandra43 <addanapurisharathchandra99@gmail.com>
2023-06-27 19:55:42 +0000

    Oyelabs ASSESSMENT

---
+++ Q2.js
@@ -0,0 +1,19 @@
+// Write a sql query for finding the subjects for each
+//student, the subjects should be order by alphabetically//
+
+//SELECT
+// CUSTOMERS.customerid,
+//  CUSTOMERS.name,
+//  (
+//    SELECT
+//      (SUBJECTS.subjectName)
+//    FROM
+//      SUBJECTS
+//      INNER JOIN SUBJECT_STUDENT_MAPPING AS m ON m.subjectid = SUBJECTS.subjectid
+//    WHERE
+//      m.customerid = CUSTOMERS.customerid
+//    ORDER BY
+//      SUBJECTS.subjectName
+//  ) AS SUBJECT
+// FROM
+///  CUSTOMERS;
